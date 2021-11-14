4ccfdf68a6a50530d6d60863228e38024ee36da6
tahadarwesh1 <tahadarwesh1@gmail.com>
2021-11-14 12:11:32 +0200

    master

--- lib/layout/news_layout.dart
+++ lib/layout/news_layout.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app/components/components.dart';
 import 'package:news_app/cubit/cubit.dart';
 import 'package:news_app/cubit/states.dart';
 import 'package:news_app/modules/search/search_screen.dart';
+import 'package:news_app/shared/components/components.dart';
 import 'package:news_app/shared/main_cubit/cubit.dart';
 
 class NewsLayout extends StatelessWidget {
--- lib/modules/entertainment/entertainment_screen.dart
+++ lib/modules/entertainment/entertainment_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app/components/components.dart';
 import 'package:news_app/cubit/cubit.dart';
 import 'package:news_app/cubit/states.dart';
+import 'package:news_app/shared/components/components.dart';
 
 class EntertainmentScreen extends StatelessWidget {
   const EntertainmentScreen({Key? key}) : super(key: key);
